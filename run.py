de17997e21a71118e6f2a41d40995f2547ef7906
Minami <table.cloth.03@gmail.com>
2019-03-26 14:38:27 +0700

    first commit

---
+++ run.py
@@ -0,0 +1,9 @@
+from slackbot.bot import Bot
+
+def main():
+    bot = Bot()
+    bot.run()
+
+if __name__ == "__main__":
+    print("Botを起動しました") # Bot起動時に表示されるメッセージ
+    main()
---
+++ slackbot_settings.py
@@ -0,0 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# .envを読み込む
+env_path = join(dirname(__file__), '.env')
+load_dotenv(env_path)
+
+# .envからbotアカウントのトークンを取得
+API_TOKEN = os.environ.get("API_TOKEN")
+
+# このBotが行う処理が見つからなかった時に返すメッセージ
+DEFAULT_REPLY = "応答に失敗しました"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名
+PLUGINS = ['plugins']
